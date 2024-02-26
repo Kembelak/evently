3e0af08f291beac92719554f74337c7dcb99f46e
Mohammad Maroufian <kembelak@gmail.com>
2024-02-26 22:06:44 +0330

    layout base created

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Evently</h1>
+      <Button variant="destructive" className="py-10">Hello</Button>
+    </main>
+  );
+}
